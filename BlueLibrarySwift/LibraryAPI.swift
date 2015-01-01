4f14ce67e706db45913e23bd59de29fbeec56093
chanju Jeon <chanju.jeon@gmail.com>
2015-01-02 02:27:01 +0900

    Use the Facade Pattern

--- BlueLibrarySwift/LibraryAPI.swift
+++ BlueLibrarySwift/LibraryAPI.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class LibraryAPI: NSObject {
+    
+    private let persistencyManager: PersistencyManager
+    private let httpClient: HTTPClient
+    private let isOnline: Bool
+    
     class var sharedInstance: LibraryAPI {
         struct Singleton {
             static let instance = LibraryAPI()
@@ -16,5 +21,29 @@ class LibraryAPI: NSObject {
         return Singleton.instance
     }
     
+    override init() {
+        persistencyManager = PersistencyManager()
+        httpClient = HTTPClient()
+        isOnline = false
+        
+        super.init()
+    }
     
+    func getAlbums() -> [Album] {
+        return persistencyManager.getAlbums()
+    }
+    
+    func addAlbum(album: Album, index: Int) {
+        persistencyManager.addAlbum(album, index: index)
+        if isOnline {
+            httpClient.postRequest("/api/addAlbum", body: album.description())
+        }
+    }
+    
+    func deleteAlbum(index: Int) {
+        persistencyManager.deleteAlbumAtIndex(index)
+        if isOnline {
+            httpClient.postRequest("/api/deleteAlbum", body: "\(index)")
+        }
+    }
 }
