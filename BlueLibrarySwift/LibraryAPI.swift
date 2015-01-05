39b684cd7ee4ea7aa1393d6fcf7092d65843ce75
chanju Jeon <chanju.jeon@gmail.com>
2015-01-05 22:46:55 +0900

    Use the Observer Pattern (Notifications, KVO)

--- BlueLibrarySwift/LibraryAPI.swift
+++ BlueLibrarySwift/LibraryAPI.swift
@@ -27,6 +27,12 @@ class LibraryAPI: NSObject {
         isOnline = false
         
         super.init()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "downloadImage:", name: "BLDownloadImageNotification", object: nil)
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
     }
     
     func getAlbums() -> [Album] {
@@ -44,6 +50,26 @@ class LibraryAPI: NSObject {
         persistencyManager.deleteAlbumAtIndex(index)
         if isOnline {
             httpClient.postRequest("/api/deleteAlbum", body: "\(index)")
+        }
+    }
+    
+    func downloadImage(notification: NSNotification) {
+        let userInfo = notification.userInfo as [String:AnyObject]
+        var imageView = userInfo["imageView"] as UIImageView?
+        let coverUrl = userInfo["coverUrl"] as NSString
+        
+        if let imageViewUnWrapped = imageView {
+            imageViewUnWrapped.image = persistencyManager.getImage(coverUrl.lastPathComponent)
+            if imageViewUnWrapped.image == nil {
+                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
+                    let downloadedImage = self.httpClient.downloadImage(coverUrl)
+                    
+                    dispatch_sync(dispatch_get_main_queue(), { () -> Void in
+                        imageViewUnWrapped.image = downloadedImage
+                        self.persistencyManager.saveImage(downloadedImage, filename: coverUrl.lastPathComponent)
+                    })
+                })
+            }
         }
     }
 }
